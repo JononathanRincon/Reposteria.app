61dd89bea0402249bbab7634cbb92c2b30e83646
Leonel Coronado Padilla <62271362+leonel-lcp@users.noreply.github.com>
2022-11-16 08:43:18 -0500

    creacion archivos backend

--- reposteria.app.backend/db/dbConnection.js
+++ reposteria.app.backend/db/dbConnection.js
@@ -1,4 +1,6 @@
-const stringConn = `mongodb+srv://JonathanRincon:DetlF8qI25pxtIY4@cluster0.9iiqhkd.mongodb.net/PanaderiaCaldas?retryWrites=true&w=majority`
-
+const username = 'JonathanRincon'
+const pass = 'DetlF8qI25pxtIY4'
+const dataBase = 'PanaderiaCaldas'
+const stringConn = `mongodb+srv://${username}:${pass}@cluster0.9iiqhkd.mongodb.net/${dataBase}?retryWrites=true&w=majority`
 module.exports =  { stringConn } ;
 // module.exports =  stringConn  ; //exportacion por defecto
---
+++ reposteria.app.backend/models/ProductosModel.js
@@ -0,0 +1,21 @@
+//para crear un modelo, necesitas una const tipo mongoose
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+//definicion del modelo
+let UserSchema = new Schema( {
+    firstname: String,
+    lastname: String,
+    email: String,
+    password: String,
+    rol: { 
+        type: Number, 
+        default: 2 
+    },
+    creationDate: { 
+        type: Date, 
+        default: Date.now 
+    },
+} )
+
+module.exports = mongoose.model('User', UserSchema,'Users')
